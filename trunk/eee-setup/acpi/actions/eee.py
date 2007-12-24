99186e985b2a6cb26a07bbb8a0c2ae43fd9e1c94
martin.andrews <martin.andrews@a2ddaa41-573f-0410-bb0a-8d6c0fff7e5f>
2007-12-24 00:36:49 +0000

    asus_acpi works on Eee - copying back files

---
+++ trunk/eee-setup/acpi/actions/eee.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require('2.0')
+import pynotify
+import sys
+import gtk
+import os
+
+if __name__ == '__main__':
+	if not pynotify.init("acpi notification"):
+		sys.exit(1)
+	n = pynotify.Notification("Unknown","'%s'acpi event : %s" % (sys.argv[1], sys.argv[2]))
+	to=500
+	
+	if sys.argv[1] == "wifi-off":
+		#uri = "file:///usr/share/icons/gnome/scalable/devices/network-wireless.svg"
+		#uri = "file:///usr/share/icons/Mist/scalable/places/network.svg"
+		uri = "file:///usr/share/icons/Bluecurve/64x64/actions/connect_no.png"
+		n = pynotify.Notification("Wifi", "Your wireless adapter has been <b><span color='red'>disabled</span></b>. This should save power and prevent interferences in planes. Press <i>Fn-F2</i> to reenable it.", uri)
+		to=3000
+	if sys.argv[1] == "wifi-on":
+		#uri = "file:///usr/share/icons/gnome/scalable/devices/network-wireless.svg"
+		#uri = "file:///usr/share/icons/HighContrast-SVG/scalable/devices/network-wireless.svg"
+		uri = "file:///usr/share/icons/Bluecurve/64x64/actions/connect_established.png"
+		n = pynotify.Notification("Wifi", "Your wireless adapter has been <b><span color='red'>enabled</span></b>. Press <i>Fn-F2</i> to disable it.", uri)
+		to=3000
+
+	if sys.argv[1] == "resume":
+		n = pynotify.Notification("Resume", "Your Eee just resumed from a suspend")
+		to=3000
+	if sys.argv[1] == "shutdown":
+		uri = "file:///usr/share/icons/Bluecurve/64x64/actions/stop.png"
+		n = pynotify.Notification("Shutdown", "Eee is shutting down immediately", uri)
+
+	if sys.argv[1] == "vol-mute":
+		uri = "file:///usr/share/icons/Bluecurve/64x64/status/audio-volume-muted.png"
+		n = pynotify.Notification("Speaker Mute", "", uri)
+	if sys.argv[1] == "vol-unmute":
+		uri = "file:///usr/share/icons/Bluecurve/64x64/status/audio-volume-muted.png"
+		n = pynotify.Notification("Speaker Unmute", "", uri)
+	if sys.argv[1] == "vol-down":
+		uri = "file:///usr/share/icons/Bluecurve/64x64/status/audio-volume-low.png"
+		n = pynotify.Notification("Volume - Decrease", "", uri)
+	if sys.argv[1] == "vol-up":
+		uri = "file:///usr/share/icons/Bluecurve/64x64/status/audio-volume-high.png"
+		n = pynotify.Notification("Volume - Increase", "", uri)
+
+	if sys.argv[1] == "vga":
+		#uri = "file:///usr/share/icons/Bluecurve/64x64/status/audio-volume-high.png"
+		n = pynotify.Notification("LCD / VGA Switch", "TODO")
+
+	if sys.argv[1] == "apps":
+		#uri = "file:///usr/share/icons/Bluecurve/64x64/status/audio-volume-high.png"
+		n = pynotify.Notification("Apps Key", "Any ideas for this one?")
+		to=3000
+
+	n.set_timeout(to)
+	if not n.show():
+		print "Failed to send notification"
+		sys.exit(1)
+
